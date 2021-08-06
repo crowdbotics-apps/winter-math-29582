9b1e8098c2cf824163ccd9abf08f75077706774d
Crowdbotics <team@crowdbotics.com>
2021-08-06 22:13:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "winter_math_29582Identifier",
+  url: "https://winter_math_29582Identifier.botics.co",
+  api: "https://winter_math_29582Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
